8cdfb2aa8fb14438c4ce2bb3b8ea85ba1b7daae9
guenoel <guenoel@gmail.com>
2024-11-06 22:16:04 +0100

    add maj on controller name in route

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'index');
 
-Route::get('/devices', [\App\Http\Controllers\deviceController::class, 'index'])->name('devices.index');
-Route::get('/devices/create', [\App\Http\Controllers\deviceController::class, 'create'])->name('devices.create');
-Route::post('/devices', [\App\Http\Controllers\deviceController::class, 'store'])->name('devices.store');
-Route::get('/devices/edit/{device}', [\App\Http\Controllers\deviceController::class, 'edit'])->name('devices.edit');
-Route::put('/devices/{device}', [\App\Http\Controllers\deviceController::class, 'update'])->name('devices.update');
-Route::delete('/devices/{device}', [\App\Http\Controllers\deviceController::class, 'destroy'])->name('devices.destroy');
+Route::get('/devices', [\App\Http\Controllers\DeviceController::class, 'index'])->name('devices.index');
+Route::get('/devices/create', [\App\Http\Controllers\DeviceController::class, 'create'])->name('devices.create');
+Route::post('/devices', [\App\Http\Controllers\DeviceController::class, 'store'])->name('devices.store');
+Route::get('/devices/edit/{device}', [\App\Http\Controllers\DeviceController::class, 'edit'])->name('devices.edit');
+Route::put('/devices/{device}', [\App\Http\Controllers\DeviceController::class, 'update'])->name('devices.update');
+Route::delete('/devices/{device}', [\App\Http\Controllers\DeviceController::class, 'destroy'])->name('devices.destroy');
