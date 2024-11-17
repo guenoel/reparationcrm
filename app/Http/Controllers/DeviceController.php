b9e4c021eb003ff3fd8927fe7ef4a3ecbce03f58
guenoel <guenoel@gmail.com>
2024-11-17 17:46:10 +0100

    add username in search

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -28,7 +28,10 @@ class DeviceController extends Controller
             
             // Adding multiple fields for search
             $devices = $devices->where(function ($query) use ($searchTerm) {
-                $query->where('brand', 'like', $searchTerm)
+                $query  ->WhereHas('user', function ($userQuery) use ($searchTerm) {
+                    $userQuery->where('name', 'like', $searchTerm); // Recherche sur le nom de l'utilisateur
+                })
+                        ->orwhere('brand', 'like', $searchTerm)
                         ->orWhere('model_name', 'like', $searchTerm)
                         ->orWhere('description', 'like', $searchTerm);
             });
