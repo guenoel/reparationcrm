9d0e33f29be51a323ffacfcdc99e4aece2791d83
guenoel <guenoel@gmail.com>
2024-11-26 00:46:49 +0100

    add header for csrf tokens

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 window.axios = axios;
 
-// Automatically attach the CSRF token from cookies to all requests
+// Set default headers for Axios
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// Attach X-XSRF-TOKEN from the cookies
+// Extract the CSRF token from the cookie and set it as a default header
 const csrfToken = document.cookie
     .split('; ')
     .find(row => row.startsWith('XSRF-TOKEN='))
     ?.split('=')[1];
 
 if (csrfToken) {
-    window.axios.defaults.headers.common['X-XSRF-TOKEN'] = csrfToken;
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = decodeURIComponent(csrfToken);
+} else {
+    console.error('CSRF token not found in cookies.');
 }
