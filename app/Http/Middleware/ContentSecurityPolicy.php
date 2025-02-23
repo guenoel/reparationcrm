f78c05a0f0dbe791ca85b51b7940bb158bc20136
guenoel <guenoel@gmail.com>
2025-02-23 17:09:12 +0100

    change CSP conf

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -13,21 +13,19 @@ class ContentSecurityPolicy
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
-
-        // Ensure headers do not contain new lines
+    
         $cspPolicy = "default-src 'self';
             script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com http://127.0.0.1:5173; 
             style-src 'self' 'unsafe-inline' https://fonts.bunny.net https://cdnjs.cloudflare.com https://fonts.googleapis.com; 
             img-src 'self' data: https://trusted-images.com; 
             font-src 'self' https://fonts.gstatic.com https://fonts.bunny.net https://cdnjs.cloudflare.com; 
-            connect-src 'self' https://api.trusted-source.com ws://127.0.0.1:5173; 
-            frame-ancestors 'none'; object-src 'none'; form-action 'self'";
-
-
+            connect-src 'self' https://api.trusted-source.com ws://127.0.0.1:5173 wss://127.0.0.1:5173; 
+            frame-ancestors 'none'; object-src 'none'; form-action 'self';";
+    
         // Replace accidental new lines in headers
         $cspPolicy = preg_replace('/\s+/', ' ', $cspPolicy);
-
+    
         $response->headers->set('Content-Security-Policy', $cspPolicy);
         return $response;
-    }
+    }    
 }
